23d56cc2c55aa18d5a83ff5d772d56575bbb81f0
victow <victorwang221@gmail.com>
2025-06-22 22:04:18 -0400

    Refactoring to coroutines

--- server.cpp
+++ server.cpp
@@ -1,24 +1,39 @@
 #include "server.hpp"
 #include "session.hpp"
 
-Server::Server(boost::asio::io_context& io_context, int port)
-    :io_context(io_context),
-    endpoint(boost::asio::ip::tcp::v4(), port), 
-    acceptor(io_context, endpoint)
+using boost::asio::co_spawn;
+using boost::asio::io_context;
+using boost::asio::awaitable;
+using boost::asio::use_awaitable;
+using boost::asio::detached;
+using boost::asio::ip::tcp;
+
+Server::Server(io_context& io, int port)
+    :io(io),
+    endpoint(tcp::v4(), port), 
+    acceptor(io, endpoint)
 {}
 
-void Server::scan(Session* incoming_session){
+awaitable<void> Server::scan_helper(Session* incoming_session){
     std::cout << "Open for connections ... " << std::endl;
-    acceptor.async_accept(incoming_session->get_socket(), 
-    [this, incoming_session](const boost::system::error_code& error){
-        if (error){
-            std::cerr << "(Server) Connection Error: " << error.message() << std::endl;
-            std::cout << "Retry? (y/n)" << std::endl;
-            //checks to see if they want to retry again
-            scan(incoming_session);
-        }
-        else if (!error){
-            incoming_session->handshake();
-        }
-    });
+    try {
+        co_await acceptor.async_accept(incoming_session->get_socket(), use_awaitable);
+        incoming_session->incoming_handshake();
+    }
+    catch (const boost::system::system_error& error){
+        std::cerr << "(Server) Connection Error: " << error.what() << std::endl;
+        std::cout << "Retry? (y/n)" << std::endl;
+        //checks to see if they want to retry again (will add functionality later)
+    }
+    co_return;
 }
+
+void Server::scan(Session* incoming_session){
+    co_spawn(
+        io, 
+        [this, incoming_session]() -> awaitable<void> {
+            co_await scan_helper(incoming_session);
+        }, 
+        detached);
+}
+
--- server.hpp
+++ server.hpp
@@ -1,4 +1,8 @@
 #include <boost/asio.hpp>
+#include <boost/asio/awaitable.hpp>
+#include <boost/asio/use_awaitable.hpp>
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/this_coro.hpp>
 #include <iostream>
 
 //Forward Declaration
@@ -11,11 +15,13 @@ class Server {
     Server(boost::asio::io_context& io_context, int port);
 
     //Starts scanning for connections (asynchronous)
-    void scan(Session* session);
+    void scan(Session* incoming_session);
 
     private:
 
-    boost::asio::io_context& io_context;
+    boost::asio::awaitable<void> scan_helper(Session* incoming_session);
+
+    boost::asio::io_context& io;
     boost::asio::ip::tcp::endpoint endpoint;
     boost::asio::ip::tcp::acceptor acceptor;
 };
